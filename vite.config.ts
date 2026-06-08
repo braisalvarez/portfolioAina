2538242bf4206ccc57e926fb7e174c8780138d7a
braisalvarez <balvarezescudero@gmail.com>
2026-06-08 21:09:30 +0200

    AutoDeploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/portfolioAina/',
   plugins: [react(), tailwindcss()],
 })
